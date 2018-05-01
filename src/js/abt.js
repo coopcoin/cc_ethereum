5348e3209e14074c01410389f2bc085df9fff742
Velasquez <velasquez.af@gmail.com>
2018-05-01 00:53:42 -0500

    change truffle-contract for ethers.js as ethereum framework, make it work with metamask

--- src/js/abt.js
+++ src/js/abt.js
@@ -1,119 +1,136 @@
-var assetTokenContract;
-var ethAccount;
 var network;
-var balance;
+var provider;
+var signer;
+var ethAccount;
+var assetTokenContract;
+var tokenBalance;
 
+
+// Get an ethereum provider and a signer
 window.onload = function() {
   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
   if (typeof web3 !== 'undefined') {
     // Use Mist/MetaMask's provider
-    window.web3 = new Web3(web3.currentProvider);
+    // get network and initialize provider
+    web3.version.getNetwork((err, netId) => {
+      network = ethers.networks[getNetworkName(netId)];
+      provider = new ethers.providers.Web3Provider(web3.currentProvider, network);
+      signer = provider.getSigner();
+      $("#network").html(network.name);
+      init();
+    });
   } else {
     console.log('No web3? You should consider trying MetaMask!')
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    provider = ethers.providers.getDefaultProvider();
+    network = ethers.networks[getNetworkName(provider.chainId)];
+    init();
   }
-  $.getScript("node_modules/truffle-contract/dist/truffle-contract.js", init);
 }
+
 
 function init() {
-  // interval to check account changes or network changes
-  // also if MetaMask is locked.
-  var dataInterval = setInterval(function() {
-    // is MetaMask locked?
-    if ( web3.eth.accounts[0] === undefined) {
-      ethAccount = undefined;
-      $("#ethAccount").html("Please Unlock MetaMask");
-    }
-    else {
-      // did the account changed?
-      if (web3.eth.accounts[0] !== ethAccount) {
-        ethAccount = web3.eth.accounts[0];
-        $("#ethAccount").html(ethAccount);
-      }
-    }
-    // get and update network
-    web3.version.getNetwork((err, netId) => {
-      if (network !== netId) {
-        switch (netId) {
-          case "1":
-            network = 'mainnet';
-            break
-          case "2":
-            network = 'Morden';
-            break
-          case "3":
-            network = 'ropsten';
-            break
-          case "4":
-            network = 'Rinkeby';
-            break
-          case "42":
-            network = 'Kovan';
-            break
-          default:
-            network = 'unknown';
-        }
-        $("#network").html(network);
-        if (assetTokenContract !== undefined) {
-          assetTokenContract.deployed().then((inst) => {
-            $("#contractAddress").html(inst.address);
-          });
-        }
-      }
-    });
-  }, 100);
-  // initialize contracts
-  $.getJSON('build/contracts/AssetToken.json', function(data) {
-    // Get the necessary contract artifact file
-    //and instantiate it with truffle-contract.
-    assetTokenContract = TruffleContract(data);
-    assetTokenContract.setProvider(web3.currentProvider);
-    assetTokenContract.deployed().then(function(inst) {
-      $("#contractAddress").html(inst.address);
-    });
-    getTokens();
-    listenForEvents();
-  });
-}
-
-function listenForEvents() {
-  function handleEvent(error, result){
-    if (error) {
-      console.log(error);
+  // load ABT contract from truffle build
+  $.getJSON('build/contracts/AssetToken.json', (contractData) => {
+    if (contractData.networks[network.chainId] === undefined) {
+      $("#contractAddress").html("Contract not found in this network.");
       return;
     }
-    handleTransferEvent(result);
-  }
-  assetTokenContract.deployed().then(inst => {
-    inst.Transfer({_from:ethAccount}, {fromBlock:0,
-      toBlock: 'latest'}).watch(handleEvent);
-    inst.Transfer({_to:ethAccount}, {fromBlock:0,
-      toBlock: 'latest'}).watch(handleEvent);
+    var address = contractData.networks[network.chainId].address;
+    var abi = contractData.abi;
+    assetTokenContract = new ethers.Contract(address, abi, signer);
+    $("#contractAddress").html(assetTokenContract.address);
   });
-}
 
-function getTokens(){
-  assetTokenContract.deployed().then((inst) => {
-    return inst.balanceOf(ethAccount);
-  }).then((b) => {
-    balance = b.toString();
-    $("#balanceOf").html(balance);
-    for (var i=0; i<balance; i++) {
-      $("#mytokens").append("<tr id=tokenIndex_" + i +"><td>" + i + "</td></tr>");
-      getTokenOfOwnerByIndex(i);
+  // set loop to check for address change
+  var stateChangeInterval = setInterval(function () {
+    // check if address changed
+    if (signer !== undefined) {
+      signer.getAddress().then(address => {
+        if (address !== ethAccount) {
+          ethAccount = address;
+          setAddress();
+          assetTokenContract.balanceOf(ethAccount).then((b) => {
+            tokenBalance = b.toNumber();
+            setBalance();
+            setTokensTable();
+            getTransferHistory({live:true});
+          });
+        }
+      }).catch(err => {
+        console.log(err);
+        ethAccount = 0;
+        setAddress();
+      });
     }
-  });
+  }, 2000);
 }
 
-function getTokenOfOwnerByIndex(index) {
-  assetTokenContract.deployed().then((inst) => {
-    return inst.tokenOfOwnerByIndex(ethAccount, index);
-  }).then((tokenId) => {
-    $("#tokenIndex_" + index).append("<td>" + tokenId + "</td>");
-    $("#tokenIndex_" + index).append("<td><form id=transferForm><input type=text id=transferToAddress_" + tokenId + "><button type=button onclick=handleTransferButton(" + tokenId + ")>transfer</button></form></td>");
-  });
+
+function setBalance() {
+  $("#balanceOf").html(tokenBalance.toString());
 }
+
+
+function setTokensTable(){
+  $("#mytokens > tbody").html("");
+  for (var i=0; i < tokenBalance; i++) {
+    assetTokenContract.tokenOfOwnerByIndex(ethAccount, i).then((tokenId) => {
+      setTokensTableRow(tokenId);
+    })
+  }
+}
+
+
+function setTokensTableRow(tokenId) {
+  var rowHtml = "";
+  rowHtml += "<tr id=tokenId_" +  tokenId + ">";
+  rowHtml += "  <td>" + tokenId + "</td>";
+  rowHtml += "  <td>";
+  rowHtml += "    <form id=transferForm>";
+  rowHtml += "      <input type=text id=transferToAddress_" + tokenId + ">";
+  rowHtml += "      <button type=button onclick=handleTransferButton(" + tokenId + ")>transfer</button>";
+  rowHtml += "    </form>";
+  rowHtml += "  </td>";
+  rowHtml += "</tr>";
+  $("#mytokens > tbody").append(rowHtml);
+}
+
+
+function getNetworkName(network_id) {
+  var networkName;
+  switch (network_id) {
+    case "1":
+      networkName = 'mainnet';
+      break;
+    case "2":
+      networkName = 'morden';
+      break;
+    case "3":
+      networkName = 'ropsten';
+      break;
+    case "4":
+      networkName = 'rinkeby';
+      break;
+    case "42":
+      networkName = 'kovan';
+      break;
+    default:
+      networkName = 'unknown';
+      break;
+  }
+  return networkName;
+}
+
+
+function setAddress() {
+  if (ethAccount === 0) {
+    $("#ethAccount").html("Error loading account, maybe unlock MetaMask?");
+    return;
+  }
+  $("#ethAccount").html(ethAccount);
+}
+
 
 function handleTransferButton(tokenId) {
   var to = $("#transferToAddress_" + tokenId).val();
@@ -121,20 +138,90 @@ function handleTransferButton(tokenId) {
   transfer(ethAccount, to, tokenId)
 }
 
+
 function transfer(from, to, tokenId){
-  assetTokenContract.deployed().then(inst => {
-    return inst.safeTransferFrom(from, to, tokenId);
-  }).then(recipt => {
-    console.log(recipt);
+  assetTokenContract.safeTransferFrom(from, to, tokenId).then(tx => {
+    console.log(tx);
   }).catch(e => {
     console.log(e);
   });
 }
 
-function handleTransferEvent(log) {
-  from = log.args._from;
-  to = log.args._to;
-  tokenId = log.args._tokenId;
-  txHash = log.transactionHash;
-  $("#tx_history").append("<tr><td>Transfer</td><td>" + from + "</td><td>" + to + "</td><td>" + tokenId + "</td><td>" + txHash + "</td></tr>");
+
+function getTransactionHistory() {
+  var options = {live:false}
+  getTransactionHistory(options);
+}
+
+
+function getTransferHistory(options) {
+  $("#tx_history > tbody").html("");
+  var transferInfo = assetTokenContract.interface.events.Transfer;
+  var filter = {
+    fromBlock: 0,
+    toBlock: 'latest',
+    address: assetTokenContract.address,
+    topics: transferInfo.topics
+  };
+  provider.getLogs(filter).then((log) => {
+    for(var i=0; i < log.length; i++) {
+      handleTransferEvent(log[i]);
+    }
+    if (options.live) {
+      provider.on(transferInfo.topics, (liveLog) => handleLiveTransferEvent(liveLog));
+    }
+  });
+}
+
+
+function getEventTransferData(log) {
+  var transferInfo = assetTokenContract.interface.events.Transfer;
+  var result = transferInfo.parse(log.topics, log.data);
+  if (result._from == ethAccount || result._to == ethAccount) {
+    var eventTransferData = {
+      from: result._from,
+      to: result._to,
+      tokenId: result._tokenId.toNumber(),
+      txHash: log.transactionHash
+    };
+    return eventTransferData;
+  }
+  return false;
+}
+
+
+function handleLiveTransferEvent(liveLog) {
+  var eventTransferData = getEventTransferData(liveLog);
+  if (eventTransferData) {
+    setHistroyTableRow(eventTransferData);
+    if (eventTransferData.to == ethAccount) {
+      setTokensTableRow(eventTransferData.tokenId);
+      tokenBalance++;
+    } else {
+      $("#tokenId_" + eventTransferData.tokenId).remove();
+      tokenBalance--;
+    }
+    setBalance();
+  }
+}
+
+
+function handleTransferEvent(log){
+  var eventTransferData = getEventTransferData(log);
+  if (eventTransferData) {
+    setHistroyTableRow(eventTransferData);
+  }
+}
+
+
+function setHistroyTableRow(transferTx) {
+  var rowHtml = "";
+  rowHtml += "<tr>";
+  rowHtml += "  <td>Transfer</td>";
+  rowHtml += "  <td>" + transferTx.from + "</td>";
+  rowHtml += "  <td>" + transferTx.to + "</td>";
+  rowHtml += "  <td>" + transferTx.tokenId + "</td>";
+  rowHtml += "  <td>" + transferTx.txHash + "</td>";
+  rowHtml += "</tr>";
+  $("#tx_history > tbody").append(rowHtml);
 }
---
+++ src/js/abt_old.js
@@ -0,0 +1,140 @@
+var assetTokenContract;
+var ethAccount;
+var network;
+var balance;
+
+window.onload = function() {
+  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+  if (typeof web3 !== 'undefined') {
+    // Use Mist/MetaMask's provider
+    window.web3 = new Web3(web3.currentProvider);
+  } else {
+    console.log('No web3? You should consider trying MetaMask!')
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  }
+  $.getScript("node_modules/truffle-contract/dist/truffle-contract.js", init);
+}
+
+function init() {
+  // interval to check account changes or network changes
+  // also if MetaMask is locked.
+  var dataInterval = setInterval(function() {
+    // is MetaMask locked?
+    if ( web3.eth.accounts[0] === undefined) {
+      ethAccount = undefined;
+      $("#ethAccount").html("Please Unlock MetaMask");
+    }
+    else {
+      // did the account changed?
+      if (web3.eth.accounts[0] !== ethAccount) {
+        ethAccount = web3.eth.accounts[0];
+        $("#ethAccount").html(ethAccount);
+      }
+    }
+    // get and update network
+    web3.version.getNetwork((err, netId) => {
+      if (network !== netId) {
+        switch (netId) {
+          case "1":
+            network = 'mainnet';
+            break
+          case "2":
+            network = 'Morden';
+            break
+          case "3":
+            network = 'ropsten';
+            break
+          case "4":
+            network = 'Rinkeby';
+            break
+          case "42":
+            network = 'Kovan';
+            break
+          default:
+            network = 'unknown';
+        }
+        $("#network").html(network);
+        if (assetTokenContract !== undefined) {
+          assetTokenContract.deployed().then((inst) => {
+            $("#contractAddress").html(inst.address);
+          });
+        }
+      }
+    });
+  }, 100);
+  // initialize contracts
+  $.getJSON('build/contracts/AssetToken.json', function(data) {
+    // Get the necessary contract artifact file
+    //and instantiate it with truffle-contract.
+    assetTokenContract = TruffleContract(data);
+    assetTokenContract.setProvider(web3.currentProvider);
+    assetTokenContract.deployed().then(function(inst) {
+      $("#contractAddress").html(inst.address);
+    });
+    getTokens();
+    listenForEvents();
+  });
+}
+
+function listenForEvents() {
+  function handleEvent(error, result){
+    if (error) {
+      console.log(error);
+      return;
+    }
+    handleTransferEvent(result);
+  }
+  assetTokenContract.deployed().then(inst => {
+    inst.Transfer({_from:ethAccount}, {fromBlock:0,
+      toBlock: 'latest'}).watch(handleEvent);
+    inst.Transfer({_to:ethAccount}, {fromBlock:0,
+      toBlock: 'latest'}).watch(handleEvent);
+  });
+}
+
+function getTokens(){
+  assetTokenContract.deployed().then((inst) => {
+    return inst.balanceOf(ethAccount);
+  }).then((b) => {
+    balance = b.toString();
+    $("#balanceOf").html(balance);
+    for (var i=0; i<balance; i++) {
+      $("#mytokens").append("<tr id=tokenIndex_" + i +"><td>" + i + "</td></tr>");
+      getTokenOfOwnerByIndex(i);
+    }
+  });
+}
+
+function getTokenOfOwnerByIndex(index) {
+  assetTokenContract.deployed().then((inst) => {
+    return inst.tokenOfOwnerByIndex(ethAccount, index);
+  }).then((tokenId) => {
+    $("#tokenIndex_" + index).append("<td>" + tokenId + "</td>");
+    $("#tokenIndex_" + index).append("<td><form id=transferForm><input type=text id=transferToAddress_" + tokenId + "><button type=button onclick=handleTransferButton(" + tokenId + ")>transfer</button></form></td>");
+  });
+}
+
+function handleTransferButton(tokenId) {
+  var to = $("#transferToAddress_" + tokenId).val();
+  console.log("Transfer token id: " + tokenId + " to addres: " + to);
+  transfer(ethAccount, to, tokenId)
+}
+
+function transfer(from, to, tokenId){
+  assetTokenContract.deployed().then(inst => {
+    return inst.safeTransferFrom(from, to, tokenId);
+  }).then(recipt => {
+    console.log(recipt);
+  }).catch(e => {
+    console.log(e);
+  });
+}
+
+function handleTransferEvent(log) {
+  from = log.args._from;
+  to = log.args._to;
+  tokenId = log.args._tokenId;
+  txHash = log.transactionHash;
+  $("#tx_history").append("<tr><td>Transfer</td><td>" + from + "</td><td>" + to + "</td><td>" + tokenId + "</td><td>" + txHash + "</td></tr>");
+}
