d9cd6899488dee50e91e109948b631da307ec176
juan <eldeveloperjuan@gmail.com>
2018-06-27 16:06:03 -0500

    Coopcoin fee contract

---
+++ contracts/CoopcoinFee.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.19;
+
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract CoopcoinFee is Ownable {
+  uint256 fee;
+  uint8 public decimals;
+
+  function getFee() public view returns(uint) {
+    return fee;
+  }
+
+  function setBalance(uint256 newFee) public onlyOwner {
+    fee = newFee;
+  }
+}
